34604ae3a8737e61da0444808cba3e7de3d7f232
Renkin <906155099@qq.com>
2025-02-07 18:55:25 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -44,6 +44,7 @@ if __name__ == '__main__':
 
             # 获取账号当前状态
             result = state.json()
+            print(result)
             # 获取剩余时间
             leftdays = int(float(result['data']['leftDays']))
             # 获取账号email
